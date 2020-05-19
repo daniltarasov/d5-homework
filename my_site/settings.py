f5e904e3b82b1387569ef878e7fbce4f1e6b907a
daniltarasov <daniltarasov@yandex.ru>
2020-05-20 02:49:30 +0300

    about to deploy

--- my_site/settings.py
+++ my_site/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-# STATIC_URL = '/static/'
-STATIC_URL = '/static/base-theme-ironwood/' # почему-то в курсе так и нужно для хероку оставить так
+STATIC_URL = '/static/'
+# STATIC_URL = '/static/base-theme-ironwood/' # почему-то в курсе так и нужно для хероку оставить так
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static/') #это для хероку. что это?
