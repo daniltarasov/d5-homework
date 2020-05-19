4166da2bd981cdcdd82357fdc12ac875ad586925
daniltarasov <daniltarasov@yandex.ru>
2020-05-20 02:47:25 +0300

    about to deploy

--- my_site/settings.py
+++ my_site/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'lp_4h@=vn(09!!42ygq3a)@#n8k#9^v_3pf2+i*p9v-d9%0cjy'
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY') #для хероку
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,7 +81,6 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 """
 для локалки
 """
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
@@ -130,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-# STATIC_URL = '/static/base-theme-ironwood/' # почему-то в курсе так и нужно для хероку оставить так
+# STATIC_URL = '/static/'
+STATIC_URL = '/static/base-theme-ironwood/' # почему-то в курсе так и нужно для хероку оставить так
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/') #это для хероку. что это?
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/') #это для хероку. что это?
