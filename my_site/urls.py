0d69f48e2f372aef055c3022b4f3321bbc627ede
daniltarasov <59774430+daniltarasov@users.noreply.github.com>
2020-06-14 22:07:13 +0300

    Add files via upload

--- my_site/urls.py
+++ my_site/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include  
 from p_library import views
-# from p_library.views import AuthorEdit, AuthorList, author_create_many, books_authors_create_many
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,17 +30,3 @@ urlpatterns = [
 
 ]
 
-
-"""
-p_library/urls.py
-"""
-# from django.contrib import admin  
-# from django.urls import path  
-# from .views import AuthorEdit, AuthorList, author_create_many
-
-# app_name = 'p_library' 
-# urlpatterns = [  
-# 	path('author/create', AuthorEdit.as_view(), name='author_create'),  
-# 	path('authors', AuthorList.as_view(), name='author_list'),  
-# 	path('author/create_many', author_create_many, name='author_create_many'),  
-# ]
